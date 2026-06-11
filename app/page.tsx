5663c45690f439bb3bf4b8071ffeea015307d876
Allan4444 <cenhailun@gmail.com>
2026-06-11 18:16:15 +0800

    Add SocialFeed to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/sections/Hero'
 import Guarantees from '@/components/sections/Guarantees'
 import WhoWeWorkWith from '@/components/sections/WhoWeWorkWith'
 import Process from '@/components/sections/Process'
+import SocialFeed from '@/components/sections/SocialFeed'
 import CTA from '@/components/sections/CTA'
 
 export default function HomePage() {
@@ -11,6 +12,7 @@ export default function HomePage() {
       <Guarantees />
       <WhoWeWorkWith />
       <Process />
+      <SocialFeed />
       <CTA />
     </>
   )
